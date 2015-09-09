8903bc1265b75c891ba2a55ed79d492561c47a65
amykangweb <selene6023@gmail.com>
2015-09-09 13:45:57 -0700

    User can log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
     @user = User.new(allowed_parameters)
     if @user.save
       session[:current_user_id] = @user.id
